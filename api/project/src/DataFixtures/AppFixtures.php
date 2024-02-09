8fd09d1997c7a8722c95f46ec000e9dff11b1b46
PierreWTH <wietrichpierre@gmail.com>
2024-02-09 10:34:01 +0100

    update app config

--- api/project/src/DataFixtures/AppFixtures.php
+++ api/project/src/DataFixtures/AppFixtures.php
@@ -20,14 +20,14 @@ class AppFixtures extends Fixture
     {
         // Create a user
         $user = new User();
-        $user->setEmail("user@sfdocker.com");
+        $user->setEmail("user@resrel.com");
         $user->setRoles(["ROLE_USER"]);
         $user->setPassword($this->userPasswordHasher->hashPassword($user, "password"));
         $manager->persist($user);
         
         // Create a user with admin role
         $userAdmin = new User();
-        $userAdmin->setEmail("admin@sfdocker.com");
+        $userAdmin->setEmail("admin@resrel.com");
         $userAdmin->setRoles(["ROLE_ADMIN"]);
         $userAdmin->setPassword($this->userPasswordHasher->hashPassword($userAdmin, "password"));
         $manager->persist($userAdmin);
