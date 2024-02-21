3ef020102b21573ae840acd7a676e19d1951eaed
PierreWTH <wietrichpierre@gmail.com>
2024-02-21 22:46:54 +0100

    display users

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,26 @@
 import './App.css'
+import axios from "axios";
+import React from 'react'
 
-function App() {
-  fetch('/api/users')
+export default function App() {
+  const [users, setUsers] = React.useState([])
+  
+  React.useEffect(() => {
+    axios.get('api/users').then((response) => {
+      console.log(response.data);
+      setUsers(response.data);
+    });
+  }, []);
+
+  if (!users) return null;
+    
   return (
-      <h1>TEST</h1>
+      <div>
+        <ul>
+          {users.map(user => (
+          <li key={user.id}>{user.email}</li>
+          ))}
+        </ul>
+      </div>
   )
 }
-
-export default App
