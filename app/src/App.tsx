60247e7b1f45f03d52ffc74d74e2698858a2c316
Pierre Wietrich <wietrich@adipso.com>
2024-02-21 14:14:11 +0100

    update docker config & add proxy

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  fetch('/api')
   return (
     <>
       <div>
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>TEST</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,6 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    proxy: {
+      "/api": {
+        target: "http://www:80",
+        changeOrigin: true,
+        rewrite: (p) => p.replace(/^\/api/, ""),
+      },
+    },
     watch: {
       usePolling: true,
     },
