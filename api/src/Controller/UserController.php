4d82b4cc3b2e30409e62c0a6a960c0ed1a1a9e41
PierreWTH <wietrichpierre@gmail.com>
2024-02-25 18:38:36 +0100

    add post user without validation

--- api/src/Controller/UserController.php
+++ api/src/Controller/UserController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Services\UserService;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 #[Route('/users')]
 class UserController extends AbstractController
 {   
     #[Route(methods: 'post')]
-    public function postUser()
+    public function postUser(UserService $userService, Request $request)
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        try{
+
+            $data = $request->toArray();
+            
+            $user = $userService->postUser($data); 
+            
+            return new JsonResponse($user, 200);
+        } catch (\Exception $e) {
+            return new JsonResponse(['code' => $e->getCode(), 'message' => $e->getMessage()], 500);
+        }
+
     }
 }
