0b752660afea84763339874ccb4e13c70ca84d9d
PierreWTH <wietrichpierre@gmail.com>
2024-02-26 22:53:46 +0100

    improve navbar & app css

--- app/src/Components/Navbars/Navbar.tsx
+++ app/src/Components/Navbars/Navbar.tsx
@@ -1,26 +1,30 @@
 import React from "react";
 import { useAuth } from "../../Context/useAuth";
 import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import "./Navbar.css";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
   const { isLoggedIn, user, logoutUser } = useAuth();
   return (
-    <div>
-      <h1> Ceci est une navbar</h1>
+    <nav>
+      <div>
+        <img src={logo} alt="logo" />
+      </div>
       {isLoggedIn() ? (
-        <div>
+        <div className="links">
           <p> Bienvenue, {user?.email} </p>
           <a onClick={logoutUser}>Se déconnecter</a>
         </div>
       ) : (
-        <div>
+        <div className="links">
           <Link to="/login"> Connexion </Link>
           <Link to="/register"> Inscription </Link>
         </div>
       )}
-    </div>
+    </nav>
   );
 };
 
