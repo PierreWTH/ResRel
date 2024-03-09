05474d099692946a9c978a126ef05b666611be84
PierreWTH <wietrichpierre@gmail.com>
2024-03-09 15:09:24 +0100

    add api platform doc for user routes

---
+++ api/src/OpenApi/UserDecorator.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\OpenApi;
+
+use ArrayObject;
+use ApiPlatform\OpenApi\Model;
+use ApiPlatform\OpenApi\OpenApi;
+use ApiPlatform\OpenApi\Model\PathItem;
+use ApiPlatform\OpenApi\Model\Operation;
+use ApiPlatform\OpenApi\Factory\OpenApiFactoryInterface;
+
+class OpenApiFactory implements OpenApiFactoryInterface 
+{
+    public function __construct(private OpenApiFactoryInterface $decorated)
+    {
+    }
+
+    public function __invoke(array $context = []): OpenApi
+    {
+        $openApi = ($this->decorated)($context);
+
+        // Add the delete operation
+        $deletePathItem = $openApi->getPaths()->getPath('/users/{id}')
+            ->withDelete(
+                new Operation(
+                    operationId: 'DeleteUser',
+                    summary: 'Delete user',
+                    parameters: [
+                        new Model\Parameter(
+                            name: 'id',
+                            in: 'path',
+                            required: true,
+                            schema: [
+                                'type' => 'integer',
+                            ],
+                        ),
+                    ],
+                    tags: ['User'],
+                    responses: [
+                        '200' => [
+                            'description' => 'Delete user',
+                            'content' => [
+                                'application/json' => [
+                                    'schema' => [
+                                        '$ref' => '#/components/schemas/User',
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ],
+                    security: [],
+                )
+            );
+        $openApi->getPaths()->addPath('/users/{id}', $deletePathItem);
+        
+        // Add the post operation
+        $postPathItem = $openApi->getPaths()->getPath('/users')
+            ->withPost(
+                new Operation(
+                    operationId: 'PostUser',
+                    summary: 'Create user',
+                    requestBody: new Model\RequestBody(
+                        content: new ArrayObject([
+                            'application/json' => [
+                                'schema' => [
+                                    '$ref' => '#/components/schemas/User',
+                                ],
+                            ],
+                        ]),
+                    ),
+                    tags: ['User'],
+                    responses: [
+                        '200' => [
+                            'description' => 'Create user',
+                            'content' => [
+                                'application/json' => [
+                                    'schema' => [
+                                        '$ref' => '#/components/schemas/User',
+                                    ]
+                                ],
+                            ],
+                        ],
+                    ],
+                    security: [],
+                )
+            );
+        $openApi->getPaths()->addPath('/users', $postPathItem);
+
+        return $openApi;
+
+    }
+} 
