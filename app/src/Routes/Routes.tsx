d15c4ad9dd406e679edba7019787ddcfced19273
PierreWTH <wietrichpierre@gmail.com>
2024-03-16 10:53:31 +0100

    add routes

--- app/src/Routes/Routes.tsx
+++ app/src/Routes/Routes.tsx
@@ -4,6 +4,8 @@ import LoginPage from "../Pages/LoginPage/LoginPage";
 import HomePage from "../Pages/HomePage/HomePage";
 import RegisterPage from "../Pages/RegisterPage/RegisterPage";
 import ProtectedRoutes from "./ProtectedRoute";
+import PostsPage from "../Pages/PostsPage/PostsPage";
+import CreatePostPage from "../Pages/CreatePostPage/CreatePostPage";
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -17,6 +19,23 @@ export const router = createBrowserRouter([
           </ProtectedRoutes>
         ),
       },
+      {
+        path: "/posts",
+        element: (
+          <ProtectedRoutes>
+            <PostsPage />
+          </ProtectedRoutes>
+        ),
+      },
+      {
+        path: "/createpost",
+        element: (
+          <ProtectedRoutes>
+            <CreatePostPage />
+          </ProtectedRoutes>
+        ),
+      },
+
       { path: "login", element: <LoginPage /> },
       { path: "register", element: <RegisterPage /> },
     ],
