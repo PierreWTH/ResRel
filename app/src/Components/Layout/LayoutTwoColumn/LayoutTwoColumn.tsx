ad6ad5fa88b98d246eb5738458239f7ee1215414
PierreWTH <wietrichpierre@gmail.com>
2024-03-17 23:57:01 +0100

    add layout two colum component

---
+++ app/src/Components/Layout/LayoutTwoColumn/LayoutTwoColumn.tsx
@@ -0,0 +1,18 @@
+import "./LayoutTwoColumn.css";
+
+interface LayoutTwoColumnProps {
+  children1: React.ReactNode;
+  children2: React.ReactNode;
+}
+
+export const LayoutTwoColumn = ({
+  children1,
+  children2,
+}: LayoutTwoColumnProps) => {
+  return (
+    <section className="container">
+      <div className="column">{children1}</div>
+      <div className="column">{children2}</div>
+    </section>
+  );
+};
