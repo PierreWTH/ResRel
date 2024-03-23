9bc3f28c7b78f26c120aa45684cdd4cd13a6d2a1
PierreWTH <wietrichpierre@gmail.com>
2024-03-23 23:30:44 +0100

    improve css

--- app/src/Pages/CreatePostPage/CreatePostPage.tsx
+++ app/src/Pages/CreatePostPage/CreatePostPage.tsx
@@ -1,15 +1,22 @@
 import PostForm from "../../Components/PostForm/PostForm";
+import "./CreatePostPage.css";
 
 const Props = {};
 
 export default function CreatePostPage() {
   return (
-    <section>
+    <section className="post-page">
       <div className="header">
         <h1>Créer un post</h1>
-        <p>Commencer a partager avec la communauté ResRel ! </p>
+        <p>
+          Commencez a partager avec la communauté ResRel ! Une astuce, une
+          pensée, une vidéo à partager ? Faites nous découvrir ce qui vous
+          intéresse.{" "}
+        </p>
       </div>
-      <PostForm />
+      <div className="postform-wrapper">
+        <PostForm />
+      </div>
     </section>
   );
 }
--- app/src/Pages/PostsPage/PostsPage.tsx
+++ app/src/Pages/PostsPage/PostsPage.tsx
@@ -1,13 +1,17 @@
 import Posts from "../../Components/Posts/Posts";
+import "./PostsPage.css";
 
 const Props = {};
 
 export default function PostPage() {
   return (
-    <section>
+    <section className="post-page">
       <div className="header">
         <h1>Tous les posts</h1>
-        <p>Retrouvez ici tous les posts publiés sur l'application. </p>
+        <p>
+          Retrouvez ici tous les posts publiés sur l'application. Découvrez,
+          partagez, commentez : commencez à explorer sans attendre !
+        </p>
       </div>
       <Posts />
     </section>
